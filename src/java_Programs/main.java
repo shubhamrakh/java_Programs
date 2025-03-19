d77b4120cb513af90bee6af192973d6fd2de7d75
Shubham Rakh <shubham.rakh35@gmail.com>
2025-03-20 00:17:50 +0530

    Initial commit

---
+++ src/java_Programs/main.java
@@ -0,0 +1,13 @@
+package java_Programs;
+
+import java.util.Date;
+
+public class main {	
+	public static void main(String[] args) {
+		
+		System.out.println("-----------This is simple program----------");
+		System.out.println(new Date());
+			
+	}
+
+}
